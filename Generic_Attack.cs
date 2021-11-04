5ba5170447a63295c5674b62bd6cd1f80587abe6
Tato <47118168+wtffidy@users.noreply.github.com>
2021-11-04 10:35:30 -0700

    Add files via upload

---
+++ Generic_Attack.cs
@@ -0,0 +1,38 @@
+using System;
+using RBot;
+using System.Collections.Generic;
+
+public class Script {
+	
+	int[] SkillOrder = { 1, 2, 3, 4 };
+
+	public ScriptInterface bot => ScriptInterface.Instance;
+	public void ScriptMain(ScriptInterface bot){
+		bot.Options.SafeTimings = true;
+		bot.Options.RestPackets = true;
+		bot.Options.InfiniteRange = true;
+		
+		SkillList(SkillOrder);
+				
+		while(!bot.ShouldExit()){		
+			bot.Player.Attack("*");
+		}
+	}
+	
+	/// <summary>
+	/// Spams Skills when in combat. You can get in combat by going to a cell with monsters in it with bot.Options.AggroMonsters enabled or using an attack command against one.
+	/// </summary>
+	public void SkillList(params int[] Skillset)
+	{
+		bot.RegisterHandler(1, b =>
+		{
+			if (bot.Player.InCombat)
+			{
+				foreach (var Skill in Skillset)
+				{
+					bot.Player.UseSkill(Skill);
+				}
+			}
+		});
+	}
+}
