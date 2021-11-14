d846e80bc10d3f117ee090a2f65552c85a69b836
Tato <47118168+wtffidy@users.noreply.github.com>
2021-11-14 09:49:28 -0800

    Update Void Highlord AIO - Tato & Smooth Brain Leaf Eater.cs

--- VHL AIO/Void Highlord AIO - Tato & Smooth Brain Leaf Eater.cs
+++ VHL AIO/Void Highlord AIO - Tato & Smooth Brain Leaf Eater.cs
@@ -113,7 +113,7 @@ public class VoidHighLordAIOTesting //🥔
 		"Unidentified 13",
 		"Unidentified 24",
 		"Voucher of Nulgath (non-mem)" };
-	public in VHLQuestAmounts = { 0, 300, 0, 20, 1, 19, 0, 0, 0, 150, 0, 0, 17, 200, 0, 0, 200, 30, 15, 200, 200 };
+	public int VHLQuestAmounts = { 0, 300, 0, 20, 1, 19, 0, 0, 0, 150, 0, 0, 17, 200, 0, 0, 200, 30, 15, 200, 200 };
 	public string[] EssenceOfDefeat = { "Defeated Makai", "Escherion's Chain", "O-dokuro's Tooth", "Strand of Vath's Hair", "Aracara's Fang", "Hydra Scale", "Tibicenas' Chain", "Dark Crystal Shard", "Essence of Nulgath" };
 	public string[] KissTheVoid = { "Tendurrr The Assistant", "Fragment of Chaos", "Dark Crystal Shard", "Blood Gem of the Archfiend", "Defeated Makai", "EssenceNulgath", "Unidentified 10", "Archfiend's Favor", "Essence of Nulgath"}; // + "Broken Betrayal Blade" x8 Dropped by Legion Fenrir Level 29 - Temp
 	public string[] Merge = { "Roentgenium of Nulgath", "Unidentified 10", "Gem of Nulgath", "Dark Crystal Shard", "Tainted Gem", "Diamond of Nulgath", "Blood Gem of the Archfiend", "Totem of Nulgath", "Elders' Blood" };
