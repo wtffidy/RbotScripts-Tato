ad82543bdcfb537daa9e9f6c3e2acae41bc40237
Tato <47118168+wtffidy@users.noreply.github.com>
2021-11-18 05:31:40 -0800

    Update Void Highlord AIO - Tato & Smooth Brain Leaf Eater.cs

--- VHL AIO/Void Highlord AIO - Tato & Smooth Brain Leaf Eater.cs
+++ VHL AIO/Void Highlord AIO - Tato & Smooth Brain Leaf Eater.cs
@@ -175,6 +175,8 @@ public class VoidHighLordAIOTesting //🥔
 			while (!bot.Inventory.Contains("Voucher of Nulgath (non-mem)", 1) && !bot.Bank.Contains("Voucher of Nulgath (non-mem)", 1)) LarvaeFarm();
 			bot.Log("DarkCrystalShard");
 			while (!bot.Inventory.Contains("Dark Crystal Shard", 200) && !bot.Bank.Contains("Dark Crystal Shard", 200)) LarvaeFarm();
+			bot.Log("Diamond of Nulgath");
+			while (!bot.Inventory.Contains("Diamond of Nulgath", 1) && !bot.Bank.Contains("Diamond of Nulgath", 1)) InsertFarmHere(); // or w/e we're replacing this wit
 			bot.Log("Uni 13");
 			while (!bot.Inventory.Contains("Unidentified 13", 1) && !bot.Bank.Contains("Unidentified 13", 1)) Unidentified13();
 			bot.Log("BKOrb");
