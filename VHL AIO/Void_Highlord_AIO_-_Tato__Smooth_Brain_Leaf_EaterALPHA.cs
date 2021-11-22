e1754a09552ebf07bb08654440de27ea74f4d6c7
Smooth Brain Leaf Eater <shaunharries64@gmail.com>
2021-11-22 21:43:11 +0000

    Removed the VHL bank stop
    Combined it into one line, not sure if I am supposed to use | or ||. Will have to see.

--- VHL AIO/Void_Highlord_AIO_-_Tato__Smooth_Brain_Leaf_EaterALPHA.cs
+++ VHL AIO/Void_Highlord_AIO_-_Tato__Smooth_Brain_Leaf_EaterALPHA.cs
@@ -238,9 +238,7 @@ public class VoidHighLordAIOTesting //🥔
 		bot.Log("You're Experienced But Can You Complete It?");
 		bot.Log($"[{DateTime.Now:HH:mm:ss}] You Got The Stuff?");
 		bot.Log("VHLPurchaseCheck");
-		UnbankList(TatoLord);
-		bot.Sleep(2000);
-		if (bot.Inventory.Contains("Roentgenium of Nulgath", 15) && bot.Inventory.Contains("Void Crystal A", 1) && bot.Inventory.Contains("Void Crystal B")) TatoHighLord();
+		if (bot.Inventory.Contains("Roentgenium of Nulgath", 15) || bot.Bank.Contains("Roentgenium of Nulgath", 15) && bot.Inventory.Contains("Void Crystal A", 1) || bot.Bank.Contains("Void Crystal A", 1) && bot.Inventory.Contains("Void Crystal B") || bot.Bank.Contains("Void Crystal B", 1)) TatoHighLord();
 		bot.Log("Elder");
 		while (!bot.Inventory.Contains("Elders' Blood", 2) && !bot.Bank.Contains("Elders' Blood", 2) && bot.Quests.IsAvailable(802)) GorillaBlood();
 		bot.Log("Hadean");
@@ -839,11 +837,12 @@ public class VoidHighLordAIOTesting //🥔
 		{
 			SafeMapJoin("tercessuinotlim");
 			bot.Sleep(2000);
+			bot.Log("UnbankTatoLord");
 			UnbankList(TatoLord);
 			bot.Sleep(2000);
 			bot.Log($"[{DateTime.Now:HH:mm:ss}] Buy VHL");
 			SafePurchase("Void Highlord", 1, "tercessuinotlim", 1355);
-			StopBot();
+			StopBot("Tato HighLord puchased");
 		}
 			
 					public void MaxMePls()
